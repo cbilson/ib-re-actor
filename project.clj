74814aea2d42392d82c829b98e7530b8721e8cd3
Chris Bilson <cbilson@pobox.com>
2011-09-21 09:22:42 -0700

    fix description and add dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
-(defproject re-actor "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject re-actor "0.0.1-SNAPSHOT"
+  :description "Clojure friendly wrapper for InteractiveBrokers java API"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [clj-time "0.3.0"]
+                 ;; download this from IB and add it to your local m2
+                 ;; repo, as per leiningen's instructions
+                 [jtsclient "9.6.5"]]
+    :dev-dependencies [[midje "1.3-alpha1"]])
