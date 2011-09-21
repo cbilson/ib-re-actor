34ffb7be4162d783949da715e55a52deb6091f58
Chris Bilson <cbilson@pobox.com>
2011-09-21 09:18:16 -0700

    initial commit

---
+++ src/re_actor/core.clj
@@ -0,0 +1,1 @@
+(ns re-actor.core)
---
+++ test/re_actor/test/core.clj
@@ -0,0 +1,6 @@
+(ns re-actor.test.core
+  (:use [re-actor.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
