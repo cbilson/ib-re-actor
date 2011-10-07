0c1b784795e062116423ca4f498dd605270df652
Chris Bilson <cbilson@pobox.com>
2011-10-06 18:30:43 -0700

    add lein-exec for running example scripts

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                  ;; download this from IB and add it to your local m2
                  ;; repo, as per leiningen's instructions
                  [jtsclient "9.6.5"]]
-    :dev-dependencies [[midje "1.3-alpha2"]])
+  :dev-dependencies [[midje "1.3-alpha2"]
+                     [lein-exec "0.1"]])
