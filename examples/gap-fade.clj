0c509101f778f38545fd9b4b4c148b360838e61d
Chris Bilson <cbilson@pobox.com>
2011-11-02 08:38:16 -0700

    added a few more types of requests, but still just messing around

--- examples/gap-fade.clj
+++ examples/gap-fade.clj
@@ -1,12 +1,9 @@
-(ns ib-rec-action.examples.gap-fade
+(ns ib-re-actor.examples.gap-fade
   (:use [ib-re-actor.connection]
         [ib-re-actor.contracts]
-        [clj-time.core :only [date-time minus]])))
+        [clj-time.core :only [date-time minus]]))
 
-(defn is-end? [msg]
-  (contains? [:tick-snapshot-end :error]))
-
-(defn compute-pivots [high low close]
+(defn compute-pivots [{high :high low :low close :close}]
   (let [pp (/ (+ high low close) 3)
         r1 (- (* 2 pp) low)
         s1 (- (* 2 pp) high)
@@ -15,80 +12,47 @@
         r3 (+ high (* 2 (- pp low)))
         s3 (- low (* 2 (- high pp)))]
     {:high high :low low :close close
-     :r3 r3 :r2 r2 :r1 r1 
+     :r3 r3 :r2 r2 :r1 r1
      :pp pp
      :s1 s1 :s2 s2 :s3 s3}))
+
+(defn is-end? [msg]
+  (contains? [:tick-snapshot-end :error]))
 
 (defn warning? [msg]
   (and (contains? msg :code)
        (>= (:code msg) 2100)))
 
-(defn report-errors [finished? msg]
-  (if (= :error (:type msg))
-    (do
-      (cond
-       (contains? msg :message) (println "*** " (:message msg))
-       (contains? msg :exception) (println "*** " (.toString (:exception msg))))
-      (if (not (warning? msg))
-        (reset! finished? true))))
-  msg)
+(defn process-error [msg]
+  (cond
+   (contains? msg :message) (println "*** " (:message msg))
+   (contains? msg :exception) (println "*** " (.toString (:exception msg))))
+  (if (not (warning? msg))
+    (swap! historic-prices assoc :done? true :failed? true)))(defmulti historic-handler :type)
 
-(defn get-equity-snapshot-handler [high low close id finished? msg]
-  (if (= id (:ticker-id :msg))
-    (condp = (:type msg)
-      
-      :price-tick
-      (condp = (:field msg)
-       :high (do (println "High: " (:price msg))
-                 (reset! high (:price msg)))
-       :low (do (println "Low: " (:price msg))
-                (reset! low (:price msg)))
-       :close (do (println "Close: " (:price msg))
-                  (reset! close (:price msg)))
-       (prn "? " (:type msg) ", " (:ticker-id :msg) ": " (:price msg)))
-      
-      :tick-snapshot-end
-      (do
-        (println "finished.")
-        (reset! finished? true))))
-  msg)
+(def historic-prices (atom {}))
+(defmethod historic-handler :error [msg] (process-error msg))
+(defmethod historic-handler :price-bar [msg]
+  (swap! historic-prices assoc :bars (conj (:bars @historic-prices)) {:high (:high msg)
+                               :low (:low msg)
+                               :close (:close msg)}))
 
-(defn historic-price-handler [high low close id finished? msg]
-  (if (= id (:request-id msg))
-    (condp = (:type msg)
-
-      :price-bar
-      (do
-        (reset! high (:high msg))
-        (reset! low (:low msg))
-        (reset! close (:close msg))
-        (println "bar received. high: " @high ", low: " @low ", close: " @close))
-      
-      :complete
-      (do
-        (println "finished with historic request " id)
-        (reset! finished? true))))
-  msg)
-
-(defn print-message [msg]
-  (prn msg)
-  msg)
+(defmethod historic-handler :complete [msg]
+  (swap! historic-prices assoc :done? true))
+(defmethod historic-handler :default [msg]
+  (prn msg))
 
 (defn compute-daily-pivots [contract date]
-  (let [high (atom 0.0)
-        low (atom 0.0)
-        close (atom 0.0)
-        finished? (atom false)
-        handler (comp (partial historic-price-handler high low close 1 finished?)
-                      (partial report-errors finished?)
-                      print-message)
-        connection (connect handler)]
-    (try 
-      (request-historical-data connection 1 contract date 1 :day 1 :day :midpoint)
-      (while (not @finished?)
-        (println "waiting...")
+  (let [connection (connect historic-handler)]
+    (try
+      (reset! historic-prices {})
+      (request-historical-data connection 1 contract date 1 :day 1 :day :trades)
+      (while (not (:done? @historic-prices))
+        (print ".")
         (java.lang.Thread/sleep 250))
-      (compute-pivots @high @low @close)
+      (if (:failed? @historic-prices)
+        nil
+        (compute-pivots @historic-prices))
       (finally
        (disconnect connection)))))
 
@@ -103,8 +67,46 @@
   (println "s3: " (:s3 pivots)))
 
 (defn get-opening-trade [date]
-  (let [contract (futures-contract "YM" "ECBOT" (date-time 2011 12))
-        pivots (compute-daily-pivots contract date)
-        
-        close (:close pivots)]
-    ))
+  (let [cash-contract (index "INDU" "NYSE")
+        future (futures-contract "YM" "ECBOT" (date-time 2011 12))]
+    (if-let [pivots (compute-daily-pivots cash-contract date)]
+      (print-pivots pivots))))
+
+(def news-complete? (atom false))
+(defmulti news-handler :type)
+(defmethod news-handler :error [msg]
+  (process-error msg)
+  (if (not (warning? msg))
+    (reset! news-complete? true)))
+(defmethod news-handler :default [msg] (prn msg))
+(defmethod news-handler :update-news-bulletin [{message :message}]
+  (println message))
+
+(defn watch-news []
+  (let [connection (connect news-handler)]
+    (try
+      (reset! news-complete? false)
+      (request-news-bulletins connection)
+      (while (not @news-complete?)
+        (java.lang.Thread/sleep 100))
+      (finally (disconnect connection)))))
+
+(def fundamental-data-complete? (atom false))
+(defmulti fundamental-data-handler :type)
+(defmethod fundamental-data-handler :error [msg]
+  (process-error msg)
+  (if (not (warning? msg))
+    (reset! fundamental-data-complete? true)))
+(defmethod fundamental-data-handler :default [msg] (prn msg))
+(defmethod fundamental-data-handler :fundamental-data [{report :report}]
+  (prn report)
+  (reset! fundamental-data-complete? true))
+
+(defn get-fundamentals []
+  (let [connection (connect fundamental-data-handler)]
+    (try
+      (reset! fundamental-data-complete? false)
+      (request-fundamental-data connection -1 (equity "C" "NYSE") :summary)
+      (while (not @fundamental-data-complete?)
+        (java.lang.Thread/sleep 100))
+      (finally (disconnect connection)))))
