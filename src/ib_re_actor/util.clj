bdec8c4ef99d2c456d010deee5d063205d3e2fa8
Chris Bilson <cbilson@pobox.com>
2011-11-19 06:53:04 -0800

    add if-assoc and if-set

--- src/ib_re_actor/util.clj
+++ src/ib_re_actor/util.clj
@@ -9,3 +9,20 @@
      `(defn ~property-name
         ([x#] (~get-xform (. x# ~field-name)))
         ([x# val#] (set! (. x# ~field-name) (~set-xform val#)) x#))))
+
+(defmacro if-set
+  ([attributes key object field]
+     `(if-set ~attributes ~key ~object ~field ~identity))
+  ([attributes key object field xform]
+     `(if-let [x# (get ~attributes ~key)]
+        (set! (. ~object ~field) (~xform x#)))))
+
+(defn if-assoc
+  ([attributes key val]
+     (if val
+       (assoc attributes key val)
+       attributes))
+  ([attributes key val xform]
+     (if val
+       (assoc attributes key (xform val))
+       attributes)))
