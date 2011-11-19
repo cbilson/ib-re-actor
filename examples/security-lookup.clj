d76db35e442f47bddc5d2d235a4c8656040da50a
Chris Bilson <cbilson@pobox.com>
2011-11-19 06:54:37 -0800

    most code moved out of security lookup examle

--- examples/security-lookup.clj
+++ examples/security-lookup.clj
@@ -1,37 +1,10 @@
 (ns ib-re-actor.examples.security-lookup
-  (:use [ib-re-actor.connection]
-        [ib-re-actor.contracts]
-        [clj-time.core :only [date-time minus now]]))
+  (:use [ib-re-actor.securities]
+        [clj-time.core :only [date-time]]
+        [clojure.pprint :only [pprint]]))
 
-(defn message-handler [latch results msg]
-  (let [{type :type} msg]
-    (condp = type
-      
-      :contract-details
-      (swap! results conj (:value msg))
-      
-      :contract-details-end
-      (.countDown latch)
-      
-      :error
-      (let [{error-message :message exception :exception code :code} msg]
-        (cond
-         (not (nil? error-message)) (println "*** " error-message)
-         (not (nil? exception)) (println "*** " (.toString exception)))
-        (if (not (warning? msg))
-          (.countDown latch)
-          (prn msg)))
+(defn print-contracts-details [contract-details]
+  (pprint contract-details))
 
-      nil)))
-
-(defn lookup-security [contract]
-  (let [latch (java.util.concurrent.CountDownLatch. 1)
-        results (atom [])
-        handler (partial message-handler latch results)
-        connection (connect handler)]
-    (try
-      (request-contract-details connection 1 contract)
-      (.await latch)
-      @results
-      (finally (disconnect connection)))))
-
+(-> (lookup-security {:security-type :future :symbol "ES" :exchange "GLOBEX"})
+    print-contracts-details)
