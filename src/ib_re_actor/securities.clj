5089c7a462b7e39c3f17262d96695af799e159d7
Chris Bilson <cbilson@pobox.com>
2012-01-03 07:12:37 -0800

    fix lookup-contract to use CountDownLatch instead of the hacky way it worked before

--- src/ib_re_actor/securities.clj
+++ src/ib_re_actor/securities.clj
@@ -17,37 +17,32 @@
       (if (error? msg)
         (do
           (reset! error msg)
-          (.countDown latch)
-          ))
+          (.countDown latch)))
 
-      nil
-      )))
+      nil)))
 
-(defmulti lookup-security class)
+(defn lookup-contract
+  ([attributes]
+     (lookup-security attributes 1))
+  ([attributes client-id]
+     (let [contract (translate-to-ib-contract attributes)
+           latch (java.util.concurrent.CountDownLatch. 1)
+           results (atom [])
+           error (atom nil)
+           handler (partial message-handler latch results error)
+           connection (connect handler client-id)]
+       
+       (try
+         (request-contract-details connection 1 contract)
+         (.await latch)
+         (finally (disconnect connection)))
 
-(defmethod lookup-security clojure.lang.IPersistentMap
-  [attributes]
-  (-> (translate-to-ib-contract attributes)
-      (lookup-security)))
-
-(defmethod lookup-security com.ib.client.Contract
-  [contract]
-  (let [latch (java.util.concurrent.CountDownLatch. 1)
-        results (atom [])
-        error (atom nil)
-        handler (partial message-handler latch results error)
-        connection (connect handler)]
-    (try
-      (request-contract-details connection 1 contract)
-      (.await latch)
-      (finally (disconnect connection)))
-
-    (if (not (nil? @error))
-      (let [{error-message :message exception :exception code :code} @error]
-        (cond
-         (not (nil? error-message)) (println "*** " error-message)
-         (not (nil? exception)) (println "*** " (.toString exception))
-         :default (prn @error))
-        @error)
-      @results)))
+       (if (not (nil? @error))
+         (let [{error-message :message exception :exception code :code} @error]
+           (cond
+            (not (nil? error-message)) (println "*** " error-message)
+            (not (nil? exception)) (println "*** " (.toString exception))
+            :default (prn @error))
+           @error)
+         @results))(lookup-security client-id)))
 
