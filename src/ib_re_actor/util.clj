c0cf958570997b65cb977d61127ef52db07ddb21
Chris Bilson <cbilson@pobox.com>
2012-04-25 06:18:51 -0700

    add :read-only option to field-props

--- src/ib_re_actor/util.clj
+++ src/ib_re_actor/util.clj
@@ -1,9 +1,13 @@
 (ns ib-re-actor.util
   (:use [ib-re-actor.translation]))
 
+(def ^:const option-flag-keywords #{:read-only})
+
 (defmacro field-props [& property-descriptors]
-  (reduce (fn [implementation [name field & kw-args]]
-            (let [{:keys [translation]} (apply hash-map kw-args)
+  (reduce (fn [implementation [name field & options]]
+            (let [option-flags (set (take-while option-flag-keywords options))
+                  kw-args (drop-while option-flag-keywords options)
+                  {:keys [translation]} (apply hash-map kw-args)
                   this (gensym "this")
                   val (gensym "val")]
               (assoc implementation (keyword name)
@@ -12,12 +16,13 @@
                            ~(if translation
                               `(translate :from-ib ~translation (. ~this ~field))
                               `(. ~this ~field)))
-                        ([~this ~val]
-                           (set! (. ~this ~field)
-                                 ~(if translation
-                                    `(translate :to-ib ~translation ~val)
-                                    val))
-                           ~this)))))
+                        ~@(if (not (option-flags :read-only))
+                            `(([~this ~val]
+                                 (set! (. ~this ~field)
+                                       ~(if translation
+                                          `(translate :to-ib ~translation ~val)
+                                          val))
+                                 ~this)))))))
           {} property-descriptors))
 
 (defprotocol Mappable
