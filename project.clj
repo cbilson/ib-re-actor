d2804c63bfc93e033fad2c1d93c125f6026c7d09
Chris Bilson <cbilson@pobox.com>
2012-10-09 05:29:22 -0700

    update to jtsclient 9.68; add docs for installing jtsclient jar

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject ib-re-actor "0.1.0-SNAPSHOT"
   :description "Clojure friendly wrapper for InteractiveBrokers java API"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-time "0.4.0"]
-                 ;; download this from IB and add it to your local m2
-                 ;; repo, as per leiningen's instructions
-                 [jtsclient "9.6.5"]]
+                 [clj-time "0.4.4"]
+                 [com.ib/jtsclient "9.68"]]
   :dev-dependencies [[midje "1.3.1"]
                      [lein-exec "0.1"]
                      [lein-marginalia "0.6.1"]]
