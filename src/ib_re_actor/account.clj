f6a7b86d6b40685914a1a5827963499e2a4244e6
Chris Bilson <cbilson@pobox.com>
2012-10-16 20:19:24 -0700

    add account and positions modules

---
+++ src/ib_re_actor/account.clj
@@ -0,0 +1,18 @@
+(ns ib-re-actor.account
+  (:require [ib-re-actor.gateway :as g]))
+
+(def account-details (atom nil))
+
+(defn update-account-details [{:keys [type key value currency]}]
+  (case type
+    :update-account-value
+    (swap! account-details assoc key
+           (if (nil? currency)
+             value
+             (vector value currency)))
+
+    :update-account-time
+    (swap! account-details assoc :last-updated value)))
+
+(do
+  (g/subscribe update-account-details))
---
+++ src/ib_re_actor/positions.clj
@@ -0,0 +1,14 @@
+(ns ib-re-actor.positions
+  (:require [ib-re-actor.gateway :as g]))
+
+(def positions (atom nil))
+
+(defn handle-portfolio-update [{:keys [type contract] :as msg}]
+  (when (= type :update-portfolio)
+    (swap! positions assoc contract
+           (select-keys msg [:position :market-price :market-value
+                             :average-cost :unrealized-gain-loss
+                             :realized-gain-loss]))))
+
+(do
+  (g/subscribe handle-portfolio-update))
