2146c7de4bf66a62b00c9fc5109a64fcc4a7488a
Chris Bilson <cbilson@pobox.com>
2012-10-18 08:37:35 -0700

    remove macros from synchronous and re-implement everything in terms of reduce-responses

--- src/ib_re_actor/synchronous.clj
+++ src/ib_re_actor/synchronous.clj
@@ -9,73 +9,15 @@
             [clojure.tools.logging :as log]
             [clj-time.core :as t]))
 
-(defmacro subscribe-for
-  "Given a function to call and a predicate, subscribes to the current connection
-   and returns the first message that matches the predicate, unsubscribing once
-   a message has been received."
-  [fn pred]
-  `(let [res# (promise)
-         handler# (fn [msg#]
-                    (log/info "Received: " msg#)
-                    (cond
-                     (~pred msg#) (deliver res# msg#)
-                     (g/is-end? msg#) (deliver res# msg#)))]
-     (g/subscribe handler#)
-     (try
-       (~fn)
-       (log/info "waiting for response")
-       @res#
-       (finally
-         (log/info "unsubscribing")
-         (g/unsubscribe handler#)))))
-
-(defmacro get-response
-  [request-fn & args]
-  `(let [res# (promise)
-         req-id# (g/get-request-id)
-         handler# (fn [{msg-req-id# :request-id :as msg#}]
-                    (log/info "Received: " msg#)
-                    (cond
-                     (= req-id# msg-req-id#) (deliver res# msg#)
-                     (g/is-end? msg#) (deliver res# msg#)))]
-     (g/subscribe handler#)
-     (try
-       (-> req-id# (~request-fn ~@args))
-       (log/info "waiting for response")
-       @res#
-       (finally (g/unsubscribe handler#)))))
-
-(defmacro get-responses [request-fn & args]
-  `(let [res# (future)
-         accum# (atom nil)
-         req-id# (g/get-request-id)
-         handler# (fn [{msg-req-id# :request-id type# :type :as msg#}]
-                    (log/info "Received: " msg#)
-                    (when (= msg-req-id# req-id#)
-                      (swap! accum# conj msg#)
-                      (when (g/is-end? msg#)
-                        (deliver res# @accum#))))]
-     (g/subscribe handler#)
-     (try
-       (-> req-id# (~request-fn ~@args))
-       (log/info "waiting for response")
-       @res#
-       (finally (g/unsubscribe handler#)))))
-
-(defn get-time []
-  (subscribe-for g/request-current-time
-                 #(= :current-time (:type %))))
-
-(defn get-contract-details [contract]
-  (get-response g/request-contract-details contract))
-
 (defn reduce-responses [accept? reduce-fn done? completion-fn subscribe-fn & args]
   "Given some functions, subscribes to the message stream, reducing each message into
    an accumulator, then cleans up when done."
   (let [responses (atom nil)
         result (promise)
         handler (fn [msg]
-                  (when (accept? msg)
+                  (when (if (fn? accept?)
+                          (accept? msg)
+                          true)
                     (if (g/is-end? msg)
                       (deliver result msg)
                       (swap! responses reduce-fn msg))))
@@ -92,8 +34,16 @@
         (log/debug "unsubscribing")
         (g/unsubscribe handler)
         (log/debug "completing")
-        (completion-fn)
+        (if (fn? completion-fn)
+          (completion-fn))
         (log/debug "completed")))))
+
+(defn get-time []
+  (reduce-responses #(= :current-time (:type %)) conj (comp not empty?) nil g/request-current-time))
+
+(defn get-contract-details [contract]
+  (let [req-id (g/get-request-id)]
+    (reduce-responses #(= req-id (:request-id %)) conj (comp not empty?) nil g/request-contract-details req-id contract)))
 
 (defn get-current-price [con]
   (let [fields [:open-tick :bid-price :close-price :last-size :low :ask-size :bid-size :last-price :ask-price :high :volume]
@@ -105,31 +55,34 @@
                 (let [received-fields (-> accum keys set)]
                   (every? received-fields fields)))
         complete (partial g/cancel-market-data con)]
-    (reduce-responses accept? reduce-fn done? complete g/request-market-data con)))
+    (reduce-responses accept? reduce-fn done? complete
+                      g/request-market-data con)))
 
 (defn execute-order [contract order]
   (let [order-id (g/get-order-id)
         accept? #(= order-id (:order-id %))
-        reduce-fn conj
-        done? #(some (fn [{:keys [type status]}]
-                       (and (= :order-status type) (= :filled status))))
-        complete (fn [])]
-    (reduce-responses accept? reduce-fn done? complete g/place-order contract order)))
+        reduce-fn (fn [coll x] (log/debug x) (conj coll x))
+        done? (partial some (fn [{:keys [type status]}]
+                       (and (= :order-status type) (= :filled status))))]
+    (log/debug "Order-Id: " order-id)
+    (reduce-responses accept? reduce-fn done? nil g/place-order
+                      order-id contract order)))
 
-(defn get-historic-prices [contract end-time duration duration-unit bar-size
-                           bar-size-unit what-to-show use-regular-trading-hours?]
+(defn get-historical-data [contract end-time duration duration-unit bar-size
+                           bar-size-unit what-to-show
+                           use-regular-trading-hours?]
   (let [request-id (g/get-request-id)
         accept? #(= request-id (:request-id %))
         reduce-fn conj
         done? (partial some #(= :price-bar-complete (:type %)))
         complete (fn [])
         responses (reduce-responses accept? reduce-fn done? complete
-                                    g/request-open-orders request-id
-                                    contract end-time duration-unit
+                                    g/request-historical-data request-id
+                                    contract end-time duration duration-unit
                                     bar-size bar-size-unit what-to-show
                                     use-regular-trading-hours?)
         errors (filter #(= :error (:type %)) responses)]
-    (if errors
+    (if (not (empty? errors))
       errors
       (->> responses
            (filter #(= :price-bar (:type %)))
