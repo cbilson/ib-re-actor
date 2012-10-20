4b21dbac150ef59e7c5df066d2b6302bf05cd9e1
Chris Bilson <cbilson@pobox.com>
2012-10-20 10:17:19 -0700

    fix logging

--- project.clj
+++ project.clj
@@ -3,9 +3,5 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.4"]
                  [com.ib/jtsclient "9.68"]
-                 [org.clojure/tools.logging "0.2.3"]]
-  :dev-dependencies [[midje "1.3.1"]
-                     [lein-exec "0.1"]
-                     [lein-marginalia "0.6.1"]
-                     [log4j "1.2.16"]]
-  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
+                 [org.clojure/tools.logging "0.2.3"]
+                 [clj-logging-config "1.9.10"]])
