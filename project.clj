112de27366cc703dccf381c887223de282133640
Chris Bilson <cbilson@pobox.com>
2012-10-20 23:53:27 -0700

    remove snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ib-re-actor "0.1.0-SNAPSHOT"
+(defproject ib-re-actor "0.1.0"
   :description "Clojure friendly wrapper for InteractiveBrokers java API"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.4"]
