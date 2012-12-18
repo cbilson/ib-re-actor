9ce29fda1428f2e0ead2118bcfd0a49c0bebb0f7
Chris Bilson <cbilson@pobox.com>
2012-12-18 14:27:08 -0800

    fix up mapping tests - but still more work to do here

--- test/ib_re_actor/test/mapping.clj
+++ test/ib_re_actor/test/mapping.clj
@@ -4,58 +4,124 @@
         [midje.sweet])
   (:import [com.ib.client Contract ContractDetails Order]))
 
-(def con (Contract.))
-(do (set! (.m_expiry con) "201109"))
+(defmacro defmappingtest
+  "Checking all these fields is pretty tedious, but important and defining the mappings
+is error-prone, so I think it's important that we test them. To relieve the tedium, I
+will just define a macro that generates the tests in the form I was using before.
 
-(future-fact "I can get and set the attributes of contracts"
-  (expiry (expiry con (date-time 2011 12))) => (date-time 2011 12)
-  (underlying-symbol (underlying-symbol con "foo")) => "foo"
-  (local-symbol (local-symbol con "bar")) => "bar"
-  (multiplier (multiplier con 42.5)) => 42.5
-  (exchange (exchange con "NYBOT")) => "NYBOT"
-  (currency (currency con "USD")) => "USD"
-  (primary-exchange (primary-exchange con "GLOBEX")) => "GLOBEX"
-  (include-expired? (include-expired? con true)) => true
-  (contract-id (contract-id con 54321)) => 54321
-  (security-id-type (security-id-type con :isin)) => :isin
-  (security-id (security-id con "US1234567890")) => "US1234567890")
+Specs are a vector of:
+  <<map key>>
+  <<field name>>
+  <<expected map value>>
+  <<expected object value (or omit if same as expected map value)>>
 
-(future-fact "I can create futures contracts"
-  (let [con (futures-contract "TF" "NYBOT" (date-time 2011 12))]
-    con => (fn [x] (= (date-time 2011 12) (expiry x)))
-    con => (fn [x] (= "TF" (underlying-symbol x)))
-    con => (fn [x] (= "NYBOT" (exchange x)))))
+You can have duplicate rows for the same field<->map key to try out different values"
+  [type & specs]
+  (let [obj (gensym "object")
+        map-obj (gensym "map")]
+    `(do
+    ;;; it's kind of handy to be able to look at an example sometimes
+       (def ~(symbol (str "example-" type))
+         (let [~obj (new ~type)]
+           ~@(for [[k field mv ov] specs]
+               `(set! (. ~obj ~field) ~(or ov mv)))
+           ~obj))
 
-(future-fact "I can create index contracts"
-  (let [con (index "TF" "NYBOT")]
-    con => (fn [x] (= "TF" (underlying-symbol x)))
-    con => (fn [x] (= "NYBOT" (exchange x)))))
+       (def ~(symbol (str "example-" type "-map"))
+         ~(zipmap (map #(% 0) specs)
+                  (map #(% 2) specs)))
 
-(future-fact "it has a low level interface to create limit orders"
-  (limit-order ..contract.. :buy 100 25.6 42) =>
-  (fn [x] (and (= "BUY" (.m_action x))
-              (= "LMT" (.m_orderType x))
-              (= 100 (.m_totalQuantity x))
-              (= 25.6 (.m_lmtPrice x))
-              (= "DAY" (.m_tif x))
-              (= 42 (.m_clientId x)))))
+       (fact ~(str "mapping " type)
+             (fact "object -> map"
+                   (let [~obj (new ~type)]
+                     ~@(for [[k field mv ov] specs]
+                         `(set! (. ~obj ~field) ~(or ov mv)))
+                     (let [~map-obj (->map ~obj)]
+                       ~@(for [[k field mv _] specs]
+                           `(fact ~(str field " maps to " k)
+                                  (~map-obj ~k) => ~mv)))))
+             (fact "map -> object"
+                   (let [~map-obj ~(zipmap (map #(% 0) specs)
+                                           (map #(% 2) specs))
+                         ~obj (map-> ~type ~map-obj)]
+                     ~@(for [[k field mv ov] specs]
+                         `(fact ~(str k " maps to " field)
+                                (. ~obj ~field) => ~(or ov mv)))))))))
 
-(future-fact "it has a higher level interface that makes it easier to create buy limit orders specifically"
-  (buy-limit ..contract.. 100 25.6) =>
-  (fn [x] (and (= "BUY" (.m_action x))
-              (= "LMT" (.m_orderType x))
-              (= 100 (.m_totalQuantity x))
-              (= 25.6 (.m_lmtPrice x))
-              (= "DAY" (.m_tif x)))))
+(comment
+  (clojure.pprint/pprint (macroexpand-1 '(defmappingtest Thingy [:id m_id 1 "foo"])))
+  )
 
-(future-fact "it lets me set time in force to a logical unit"
-  (let [order (-> (Order.) (time-in-force :day))]
-    (.m_tif order) => "DAY"))
 
-(future-fact "it lets me set good till date on orders with just a date"
-  (let [order (-> (Order.) (time-in-force (date-time 2011 1 2 3 4)))]
-    (.m_tif order) => "GTD"
-    (.m_goodTillDate order) => "20110102 03:04:00 UTC"))
+(defmappingtest Contract
+  [:contract-id m_conId 1]
+  [:symbol m_symbol "some symbol"]
+  [:exchange m_exchange "some exchange"]
+  [:local-symbol m_localSymbol "some local symbol"]
+  [:primary-exchange m_primaryExch "some primary exchange"]
+  [:currency m_currency "some currency"]
+  [:type m_secType :equity "STK"]
+  [:include-expired? m_includeExpired true]
+  [:security-id-type m_secIdType :isin "ISIN"]
+  [:security-id m_secId "AB1234567890"]
+  [:combo-legs-description m_comboLegsDescrip "some description of combo legs"]
+  [:expiry m_expiry (date-time 2000 1) "200001"]
+  [:multiplier m_multiplier 234.567 "234.567"]
+  [:put-call-right m_right :put "PUT"])
 
-(future-fact "I can set an order good for day"
-  (.m_tif (good-for-day (Order.))) => "DAY")
+(defmappingtest ContractDetails
+  ;; TODO: figure out what to do about m_summary
+  [:market-name m_marketName "some market name"]
+  [:trading-class m_tradingClass "some trading class"]
+  [:min-tick m_minTick 2.]
+  [:price-magnifier m_priceMagnifier 4]
+  [:order-types m_orderTypes [:limit :market] "LMT,MKT"]
+  [:valid-exchanges m_validExchanges ["GLOBEX" "ECBOT"] "GLOBEX,ECBOT"]
+  [:underlying-contract-id m_underConId 9876]
+  [:long-name m_longName "some long name"]
+  [:cusip m_cusip "459200101"]
+  [:ratings m_ratings "some credit ratings"]
+  [:description-details m_descAppend "some more description stuff"]
+  [:bond-type m_bondType "some bond type"]
+  [:coupon-type m_couponType "some coupon type"]
+  [:callable? m_callable true]
+  [:putable? m_putable true]
+  [:coupon m_coupon 1.23]
+  [:convertible? m_convertible true]
+  [:maturity m_maturity (date-time 2020 2 3) "02/03/2020"]
+  [:issue-date m_issueDate (date-time 2010 4 5) "04/05/2010"]
+  [:next-option-date m_nextOptionDate (date-time 2013 5 6) "05/06/2013"]
+  [:next-option-type m_nextOptionType "some option type"]
+  [:next-option-partial m_nextOptionPartial true]
+  [:notes m_notes "some notes"]
+  [:contract-month m_contractMonth "some contract month"]
+  [:industry m_industry "some industry"]
+  [:category m_category "some category"]
+  [:subcategory m_subcategory "some sub-category"]
+  [:time-zone-id m_timeZoneId "some time zone id"]
+  [:trading-hours m_tradingHours "some trading hours"]
+  [:liquid-hours m_liquidHours "some liquid hours"])
+
+(defmappingtest Order
+  [:order-id m_orderId 1]
+  [:client-id m_clientId 2]
+  [:permanent-id m_permId 3]
+  [:transmit? m_transmit true]
+  [:quantity m_totalQuantity 4]
+  [:action m_action :buy "BUY"]
+  [:type m_orderType :limit "LMT"]
+  [:block-order? m_blockOrder true]
+  [:sweep-to-fill? m_sweepToFill true]
+  [:time-in-force m_tif :day "DAY"]
+  [:good-after-time m_goodAfterTime "20121213 23:59:59"]
+  [:good-till-date m_goodTillDate "20121214 23:59:59"]
+  [:outside-regular-trading-hours? m_outsideRth true]
+  [:hidden? m_hidden true]
+  [:all-or-none? m_allOrNone true]
+  [:limit-price m_lmtPrice 23.99]
+  [:discretionary-amount m_discretionaryAmt 1.99]
+  [:stop-price m_auxPrice 24.99])
+
+;;; TODO: test the other mappings
+
+;;; TODO: some reflection thing to test OrderState (since there is no public ctor)
