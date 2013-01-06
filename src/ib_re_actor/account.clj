66f79f1875c146a4d5ead5c0b50877dc96a8e04e
Edmund Jackson <edmundsjackson@gmail.com>
2013-01-06 10:29:38 +0000

    Added first snippets of documentation. #3.

--- src/ib_re_actor/account.clj
+++ src/ib_re_actor/account.clj
@@ -1,9 +1,15 @@
+;; # Account details
+;; This namespace deals monitoring and updating account details.
+;;
 (ns ib-re-actor.account
   (:require [ib-re-actor.gateway :as g]))
 
+;; This atom contains the account details at all times
 (defonce account-details (atom nil))
 
-(defn update-account-details [{:keys [type key value currency]}]
+(defn update-account-details
+  "Update a partical key in the account details"
+  [{:keys [type key value currency]}]
   (case type
     :update-account-value
     (swap! account-details assoc key
@@ -14,5 +20,6 @@
     :update-account-time
     (swap! account-details assoc :last-updated value)))
 
+;; Subscribe immediately to receiving updates
 (do
   (g/subscribe update-account-details))
--- src/ib_re_actor/gateway.clj
+++ src/ib_re_actor/gateway.clj
@@ -1,12 +1,38 @@
+;; # The Gateway
+;; The main user interface. Functions for connecting to Interactive Brokers TWS Gateway and sending
+;; requests to it.
+;;
+;; ## Big Picture
+;; The IB API requires consumers to instantate an object of type com.ib.client.EClientSocket
+;; which is a socket client to the gateway, or TWS.  When instantating this object you pass in
+;; another object which must implement the com.ib.client.EWrapper interface.  The EWrapper is a
+;; collection of callbacks. You then invoke
+;; methods (like requesting price data) on the the EClientSocket object and the results
+;; (price update events) is returned by the EWrapper callbacks.
+;; So the typical pattern for dealing with the IB API is to implement an EWrapper type object for each
+;; application, which requires a lot of knowledge of the API.
+;;
+;; This library takes a slightly different approach to try ease that burden.  The library implements a listener framework
+;; around each callback in the EWrapper object.  So what happens is that each time the IB Gateway
+;; calls back to an method in the EWrapper, our object parses the response and packages it up into a tidy Clojure map
+;; which its hands to any registered listeners.
+;;
+;; Consumers of this library thus do not need to care about the mechanics of EWrapper, ESocketClient etc,
+;; they simply need to register a listener and will receive events.
+;;
+;; ## Basic Usage
+;; 1. Connect to a running Gateway of TWS, using the connect function
+;; 2. Register a listener for an event using the subscribe function
+;; 3. Request the generation of the appropriate event using the request-* functions
+;; 4. Cancel the request with the cancel-* when done.
+;;
 (ns ib-re-actor.gateway
-     "Functions for connecting to Interactive Brokers TWS Gateway and sending
-requests to it."
-     (:use [ib-re-actor.translation :only [translate integer-account-value?
-                                           numeric-account-value?
-                                           boolean-account-value?]]
-           [ib-re-actor.mapping])
-     (:require [clojure.xml :as xml]
-               [clojure.tools.logging :as log]))
+  (:use [ib-re-actor.translation :only [translate integer-account-value?
+                                        numeric-account-value?
+                                        boolean-account-value?]]
+        [ib-re-actor.mapping])
+  (:require [clojure.xml :as xml]
+            [clojure.tools.logging :as log]))
 
 (defn- get-stack-trace [ex]
   (let [sw (java.io.StringWriter.)
