327e291bc19442e6f46c69281ca7660a0e14e589
Edmund Jackson <edmundsjackson@gmail.com>
2013-02-06 18:29:27 +0000

    Bump to snapshot for work.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ib-re-actor "0.1.0"
+(defproject ib-re-actor "0.1.1-SNAPSHOT"
   :description "Clojure friendly wrapper for InteractiveBrokers java API"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.4"]
