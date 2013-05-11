ec39a07491bbb27ae801cfbab7042c09c5e3b6ff
Chris Bilson <cbilson@pobox.com>
2013-05-10 19:13:26 -0700

    rewrite synchronous functions to be simpler and work
    It was too hard to follow what was going on when using multiple function arguments

--- src/ib_re_actor/synchronous.clj
+++ src/ib_re_actor/synchronous.clj
@@ -5,128 +5,134 @@
    interactive context (such as when using the REPL) but probably not what you
    would want to use in an application, as the asynchronous API is a much more
    natural fit for building programs that react to events in market data."
-  (:require [ib-re-actor.gateway :as g]
+  (:require [ib-re-actor.gateway :refer [with-subscription end? error-end? request-end?] :as g]
             [clojure.tools.logging :as log]
             [clj-time.core :as t]))
-
-(defn- reduce-responses [accept? reduce-fn done? completion-fn subscribe-fn & args]
-  "Given some functions, subscribes to the message stream, reducing each message into
-   an accumulator, then cleans up when done."
-  (let [responses (atom nil)
-        result (promise)
-        handler (fn [msg]
-                  (when (if (fn? accept?)
-                          (accept? msg)
-                          true)
-                    (if (g/is-end? msg)
-                      (deliver result msg)
-                      (swap! responses reduce-fn msg))))
-        response-watch (fn [_ k _ v]
-                         (when (done? v)
-                           (remove-watch responses k)
-                           (deliver result v)))]
-    (add-watch responses :result-watch response-watch)
-    (g/subscribe handler)
-    (try
-      (apply subscribe-fn args)
-      @result
-      (finally
-        (log/debug "unsubscribing")
-        (g/unsubscribe handler)
-        (log/debug "completing")
-        (if (fn? completion-fn)
-          (completion-fn))
-        (log/debug "completed")))))
 
 (defn get-time
   "Returns the server time"
   []
-  (reduce-responses #(= :current-time (:type %))
-                    conj (comp not empty?) nil
-                    g/request-current-time))
+  (let [result (promise)
+        handler (fn [{:keys [type value] :as msg}]
+                  (cond
+                   (and (= type :current-time))
+                   (deliver result value)
+
+                   (error-end? msg)
+                   (deliver result msg)))]
+    (with-subscription handler
+      (g/request-current-time)
+      @result)))
 
 (defn get-contract-details
   "Gets details for the specified contract."
-  [contract]
-  (let [req-id (g/get-request-id)]
-    (reduce-responses #(= req-id (:request-id %))
-                      conj (comp not empty?) nil
-                      g/request-contract-details
-                      req-id contract)))
+  [con]
+  (let [responses (atom nil)
+        req-id (g/get-request-id)
+        results (promise)
+        handler (fn [{:keys [type request-id value] :as msg}]
+                  (cond
+                   (and (= type :contract-details) (= req-id request-id))
+                   (swap! responses conj value)
+
+                   (request-end? req-id msg)
+                   (deliver results @responses)
+
+                   (error-end? req-id msg)
+                   (deliver results msg)))]
+    (with-subscription handler
+      (g/request-contract-details req-id con)
+      @results)))
 
 (defn get-current-price
   "Gets the current price for the specified contract."
   [con]
   (let [fields (atom nil)
-        req-ticker-id (swap! g/last-ticker-id inc)
+        req-ticker-id (g/get-request-id)
         result (promise)
         handler (fn [{:keys [type ticker-id request-id field value code] :as msg}]
                   (cond
                    (and (= type :tick) (= ticker-id req-ticker-id))
                    (swap! fields assoc field value)
 
-                   (and (= type :tick-snapshot-end) (= req-ticker-id request-id))
+                   (request-end? req-ticker-id msg)
                    (deliver result @fields)
 
-                   (and (= type :error) (= 504 code))
+                   (error-end? req-ticker-id msg)
                    (deliver result msg)))]
-    (g/subscribe handler)
-    (try
+    (with-subscription handler
       (g/request-market-data con req-ticker-id "" true)
-      @result
-      (finally
-        (log/debug "unsubscribing")
-        (g/unsubscribe handler)
-        (log/debug "completing")))))
+      @result)))
 
 (defn execute-order
-  "Executes an order, returning only when the order is filled."
+  "Executes an order, returning only when the order is filled or pending."
   [contract order]
-  (let [order-id (g/get-order-id)
-        accept? #(= order-id (:order-id %))
-        reduce-fn (fn [coll x] (log/debug x) (conj coll x))
-        done? (partial some (fn [{:keys [type status]}]
-                              (and (= :order-status type)
-                                   (= :filled status))))]
-    (log/debug "Order-Id: " order-id)
-    (reduce-responses accept? reduce-fn done? nil g/place-order
-                      order-id contract order)))
+  (let [ord-id (g/get-order-id)
+        market-closed? (atom false)
+        updates (atom [])
+        result (promise)
+        handler (fn [{:keys [type request-id order-id status code message] :as msg}]
+                  (let [done (#{:filled :cancelled :inactive} status)
+                        this-order (= ord-id (or order-id request-id))]
+                    (cond
+                     (and this-order done)
+                     (deliver result (conj @updates msg))
 
-(defn get-historical-data [contract end-time duration duration-unit bar-size
-                           bar-size-unit what-to-show
-                           use-regular-trading-hours?]
+                     (and this-order @market-closed? (= :pre-submitted status))
+                     (deliver result (conj @updates msg))
+
+                     (and this-order (= :error type) (= 399 code))
+                     (do
+                       (reset! market-closed? true)
+                       (swap! updates conj msg))
+
+                     (end? ord-id msg)
+                     (deliver result (conj @updates msg))
+
+                     this-order
+                     (swap! updates conj msg))))]
+    (with-subscription handler
+      (g/place-order ord-id contract order)
+      @result)))
+
+(defn get-historical-data
   "Gets historical price bars for a contract."
-  (let [request-id (g/get-request-id)
-        accept? #(= request-id (:request-id %))
-        reduce-fn conj
-        done? (partial some #(= :price-bar-complete (:type %)))
-        complete (fn [])
-        responses (reduce-responses accept? reduce-fn done? complete
-                                    g/request-historical-data request-id
-                                    contract end-time duration duration-unit
-                                    bar-size bar-size-unit what-to-show
-                                    use-regular-trading-hours?)
-        errors (filter #(= :error (:type %)) responses)]
-    (if (not (empty? errors))
-      errors
-      (->> responses
-           (filter #(= :price-bar (:type %)))
-           (map #(select-keys % [:time :open :high :low :close
-                                 :trade-count :volume :has-gaps?]))))))
+  [contract end-time duration duration-unit bar-size bar-size-unit
+   what-to-show use-regular-trading-hours?]
+  (let [req-id (g/get-request-id)
+        accum (atom [])
+        results (promise)
+        handler (fn [{:keys [type request-id] :as msg}]
+                  (cond
+                   (end? req-id msg)
+                   (deliver results (conj @accum msg))
+
+                   (and (= req-id request-id) (= type :price-bar))
+                   (swap! accum conj msg)))]
+    (with-subscription handler
+      (g/request-historical-data req-id contract end-time duration duration-unit
+                                 bar-size bar-size-unit what-to-show use-regular-trading-hours?)
+      @results)))
 
 (defn get-open-orders
   "Gets all open orders for the current connection."
   []
-  (let [accept? #(#{:open-order :open-order-end :error} (:type %))
-        reduce-fn conj
-        done? (partial some #(= :open-order-end (:type %)))
-        complete (fn [])
-        responses (reduce-responses accept? reduce-fn done? complete
-                                    g/request-open-orders)
-        errors (filter #(= :error (:type %)) responses)]
-    (if (empty? errors)
-      (->> responses
-           (filter #(= :open-order (:type %)))
-           (map #(select-keys % [:order-id :contract :order
-                                 :order-state])))
-      errors)))
+  (let [accum (atom [])
+        results (promise)
+        handler (fn [{:keys [type] :as msg}]
+                  (cond
+                   (= :open-order type)
+                   (swap! accum conj msg)
+
+                   (error-end? msg)
+                   (deliver results (conj @accum msg))
+
+                   (end? msg)
+                   (deliver results @accum)))]
+    (with-subscription handler
+      (g/request-open-orders)
+      @results)))
+
+(defn cancel-order [order-id]
+  (g/cancel-order order-id)
+  nil)
